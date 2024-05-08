3023b0e448849772be9aba7e4fcf1200bde73a80
BryanGordon <bryanbge92@gmail.com>
2024-05-08 16:52:42 -0500

    Add more data games

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -20,5 +20,257 @@ const games: Games[] = [
       capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1174180/capsule_184x69.jpg?t=1714055653",
     },
   },
-  
+  {
+    id: "413150",
+    data: {
+      name: "Stardew Valley",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/413150/header.jpg?t=1711128146",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/413150/capsule_231x87.jpg?t=1711128146",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/413150/capsule_184x69.jpg?t=1711128146",
+    }
+  },
+  {
+    id: "292030",
+    data: {
+      name: "The Witcher® 3: Wild Hunt",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1710411171",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/292030/capsule_231x87.jpg?t=1710411171",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/292030/capsule_184x69.jpg?t=1710411171",
+    }
+  },
+  {
+    id: "1237970",
+    data: {
+      name: "Titanfall® 2",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1237970/header.jpg?t=1668565264",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1237970/capsule_231x87.jpg?t=1668565264",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1237970/capsule_184x69.jpg?t=1668565264",
+    }
+  },
+  {
+    id: "976730",
+    data: {
+      name: "Halo: The Master Chief Collection",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/976730/header.jpg?t=1670458602",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/976730/capsule_231x87.jpg?t=1670458602",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/976730/capsule_184x69.jpg?t=1670458602",
+    }
+  },
+  {
+    id: "307690",
+    data: {
+      name: "Sleeping Dogs: Definitive Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/307690/header.jpg?t=1602800785",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/307690/capsule_231x87.jpg?t=1602800785",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/307690/capsule_184x69.jpg?t=1602800785",
+    },
+  },
+  {
+    id: "742420",
+    data: {
+      name: "Saints Row",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/742420/header.jpg?t=1704729851",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/742420/capsule_231x87.jpg?t=1704729851",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/742420/capsule_184x69.jpg?t=1704729851",
+    }
+  },
+  {
+    id: "433340",
+    data: {
+      name: "Slime Rancher",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/433340/header.jpg?t=1692314094",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/433340/capsule_231x87.jpg?t=1692314094",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/433340/capsule_184x69.jpg?t=1692314094",
+    },
+  },
+  {
+    id: "597820",
+    data: {
+      name: "BIOMUTANT",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/597820/header.jpg?t=1696421418",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/597820/capsule_231x87.jpg?t=1696421418",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/597820/capsule_184x69.jpg?t=1696421418",
+    },
+  },
+  {
+    id: "1850570",
+    data: {
+      name: "DEATH STRANDING DIRECTOR'S CUT",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1850570/header.jpg?t=1713782964",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1850570/capsule_231x87.jpg?t=1713782964",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1850570/capsule_184x69.jpg?t=1713782964",
+    }
+  },
+  {
+    id: "1259420",
+    data: {
+      name: "Days Gone",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1259420/header.jpg?t=1698779323",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1259420/capsule_231x87.jpg?t=1698779323",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1259420/capsule_184x69.jpg?t=1698779323",
+    }
+  },
+  {
+    id: "1237950",
+    data: {
+      name: "STAR WARS™ Battlefront™ II",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1237950/header.jpg?t=1701356407",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1237950/capsule_231x87.jpg?t=1701356407",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1237950/capsule_184x69.jpg?t=1701356407",
+    }
+  },
+  {
+    id: "1328670",
+    data: {
+      name: "Mass Effect™ Legendary Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1328670/header.jpg?t=1669773470",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1328670/capsule_231x87.jpg?t=1669773470",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1328670/capsule_184x69.jpg?t=1669773470",
+    }
+  },
+  {
+    id: "360430",
+    data: {
+      name: "Mafia III: Definitive Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/360430/header.jpg?t=1602775991",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/360430/capsule_231x87.jpg?t=1602775991",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/360430/capsule_184x69.jpg?t=1602775991",
+    }
+  },
+  {
+    id: "894020",
+    data: {
+      name: "Death's Door",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/894020/header.jpg?t=1714110400",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/894020/capsule_231x87.jpg?t=1714110400",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/894020/capsule_184x69.jpg?t=1714110400",
+    }
+  },
+  {
+    id: "203160",
+    data: {
+      name: "Tomb Raider",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/203160/header.jpg?t=1710446431",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/203160/capsule_231x87.jpg?t=1710446431",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/203160/capsule_184x69.jpg?t=1710446431",
+    }
+  },
+  {
+    id: "1030830",
+    data: {
+      name: "Mafia II: Definitive Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1030830/header.jpg?t=1589933058",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1030830/capsule_231x87.jpg?t=1589933058",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1030830/capsule_184x69.jpg?t=1589933058",
+    }
+  },
+  {
+    id: "1604030",
+    data: {
+      name: "V Rising",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1604030/header.jpg?t=1715169294",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1604030/capsule_231x87.jpg?t=1715169294",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1604030/capsule_184x69.jpg?t=1715169294",
+    }
+  },
+  {
+    id: "1144200",
+    data: {
+      name: "Ready or Not",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1144200/header.jpg?t=1707410886",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1144200/capsule_231x87.jpg?t=1707410886",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1144200/capsule_184x69.jpg?t=1707410886",
+    }
+  },
+  {
+    id: "1030840",
+    data: {
+      name: "Mafia: Definitive Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1030840/header.jpg?t=1632420251",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1030840/capsule_231x87.jpg?t=1632420251",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1030840/capsule_184x69.jpg?t=1632420251",
+    }
+  },
+  {
+    id: "464920",
+    data: {
+      name: "Surviving Mars",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/464920/header.jpg?t=1701955588",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/464920/capsule_231x87.jpg?t=1701955588",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/464920/capsule_184x69.jpg?t=1701955588",
+    },
+  },
+  {
+    id: "730",
+    data: {
+      name: "Counter-Strike 2",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1698860631",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_231x87.jpg?t=1698860631",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_184x69.jpg?t=1698860631",
+    }
+  },
+  {
+    id: "219150",
+    data: {
+      name: "Hotline Miami",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/219150/header.jpg?t=1686261983",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/219150/capsule_231x87.jpg?t=1686261983",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/219150/capsule_184x69.jpg?t=1686261983",
+    }
+  },
+  {
+    id: "475550",
+    data: {
+      name: "Beholder",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/475550/header.jpg?t=1713508014",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/475550/capsule_231x87.jpg?t=1713508014",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/475550/capsule_184x69.jpg?t=1713508014",
+    }
+  },
+  {
+    id: "1472560",
+    data: {
+      name: "I Am Fish",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1472560/header.jpg?t=1683890027",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1472560/capsule_231x87.jpg?t=1683890027",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1472560/capsule_184x69.jpg?t=1683890027",
+    }
+  },
+  {
+    id: "234140",
+    data: {
+      name: "Mad Max",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/234140/header.jpg?t=1634068672",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/234140/capsule_231x87.jpg?t=1634068672",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/234140/capsule_184x69.jpg?t=1634068672",
+    }
+  },
+  {
+    id: "1017900",
+    data: {
+      name: "Age of Empires: Definitive Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1017900/header.jpg?t=1688053940",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1017900/capsule_231x87.jpg?t=1688053940",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1017900/capsule_184x69.jpg?t=1688053940",
+    }
+  },
+  {
+    id: "1151640",
+    data: {
+      name: "Horizon Zero Dawn™ Complete Edition",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1151640/header.jpg?t=1709156384",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1151640/capsule_231x87.jpg?t=1709156384",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1151640/capsule_184x69.jpg?t=1709156384",
+    }
+  },
+  {
+    id: "1551360",
+    data: {
+      name: "Forza Horizon 5",
+      header_image: "https://cdn.akamai.steamstatic.com/steam/apps/1551360/header.jpg?t=1714086603",
+      capsule_image: "https://cdn.akamai.steamstatic.com/steam/apps/1551360/capsule_231x87.jpg?t=1714086603",
+      capsule_imagev5: "https://cdn.akamai.steamstatic.com/steam/apps/1551360/capsule_184x69.jpg?t=1714086603",
+    }
+  }
+
 ]
