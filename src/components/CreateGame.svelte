ca7b2ba3e210471b36278df1235fafaec371f887
Bryan Gordon <bryanbge92@gmail.com>
2025-05-23 19:40:34 -0500

    Update styles

--- src/components/CreateGame.svelte
+++ src/components/CreateGame.svelte
@@ -28,15 +28,15 @@
 </script>
 
   <article>
-    <form onsubmit={handleSubmit} class="w-3/4 my-0 mx-auto h-dvh pt-4">
-      <div class="my-3 w-3/4 mx-auto">
+    <form onsubmit={handleSubmit} class="w-4/6 mb-0 mt-20 mx-auto min-h-[300px] pt-4 rounded-md bg-slate-50/95">
+      <div class="my-9 w-3/4 mx-auto">
         <label class="text-base font-medium mr-3" for="name">Nombre:</label>
-        <input placeholder='Death Stranding' type="text" name="name" id="name" required bind:value={gameName} class="rounded-xl  px-4 focus-visible:outline-none focus-visible:shadow-sm focus-visible:shadow-zinc-600" />
+        <input placeholder='Death Stranding' type="text" name="name" id="name" required bind:value={gameName} class="bg-slate-300 w-1/2 rounded-xl px-4 focus-visible:outline-none focus-visible:shadow-sm focus-visible:shadow-zinc-600 placeholder:text-zinc-500" />
       </div>
       
-      <div class="w-3/4 mx-auto my-3">
+      <div class="w-3/4 mx-auto my-9">
         <label class="text-base font-medium mr-3" for="status">Estado:</label>
-        <select name="status" id="status" bind:value={gameStatus} class="rounded-xl px-2">
+        <select name="status" id="status" bind:value={gameStatus} class="rounded-xl bg-slate-300 px-2">
           <option value='1' class="bg-red-300">Jugando</option>
           <option value='2'>Por jugar</option>
           <option value='3'>Completado</option>
@@ -44,12 +44,12 @@
         </select>
       </div>
 
-      <div class="w-3/4 mx-auto my-3">
+      <div class="w-3/4 mx-auto mt-9 mb-7">
         <label class="text-base font-medium mr-3" for="header_image">Imagen:</label>
-        <input class="rounded-xl px-4" placeholder="Link image" type="text" name="header_image" id="header_image" required bind:value={gameImage} />
+        <input class="rounded-xl px-4 bg-slate-300 placeholder:text-zinc-600 w-1/2" placeholder="Link image" type="text" name="header_image" id="header_image" required bind:value={gameImage} />
       </div>
 
-      <div class="w-1/2 my-3 mx-auto">
+      <div class="w-1/2 mx-auto">
         <button class="bg-blue-400 px-8 py-1.5 rounded-2xl" type="submit">Agregar juego</button>
       </div>
     </form>
